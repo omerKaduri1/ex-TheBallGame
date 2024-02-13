749ab52c58d1888f75e1b1ec81a9b834be6e6cd5
omerKaduri1 <kaduriomer@gmail.com>
2024-02-13 15:10:30 +0200

    Change ball size and text with onBallClick()

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 'use strict'
 
-function onBallClick() {
-    console.log('Hello');
+function onBallClick(elBall) {
+    var ballWidth = 100
+    var ballHeight = 100
+
+    ballWidth += 50
+    ballHeight += 50
+
+    elBall.style.width = ballWidth + 'px'
+    elBall.style.height = ballHeight + 'px'
+    elBall.innerText = ballHeight + ''
 }
