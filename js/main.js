f8d5a7b783a2db769761432aad0cbfaff617b47a
omerKaduri1 <kaduriomer@gmail.com>
2024-02-13 17:09:25 +0200

    Refactor ball increment to random

--- js/main.js
+++ js/main.js
@@ -1,25 +1,22 @@
 'use strict'
 
-var gBallWidth = 100
-var gBallHeight = 100
+var gBallSize= 100
 
 function onBallClick() {
     const elBall = document.querySelector('.ball')
+    const incrementAmount = getRandomInt(20, 61)
 
-    if (gBallHeight < 400) {
-        gBallWidth += 50
-        gBallHeight += 50
-    
-        elBall.style.width = gBallWidth + 'px'
-        elBall.style.height = gBallHeight + 'px'
-        elBall.innerText = gBallHeight + ''
-    }
-    
-    else {
+    gBallSize += incrementAmount
+
+    if (gBallSize + incrementAmount > 400) {
         elBall.style.width = '100px'
         elBall.style.height = '100px'
-        elBall.innerText = '100'
-        gBallHeight = 100
-        gBallWidth = 100
+        elBall.innerText = gBallSize + ''
+        gBallSize = 100
     }
+
+        elBall.style.width = gBallSize + 'px'
+        elBall.style.height = gBallSize + 'px'
+        elBall.innerText = gBallSize + ''
+
 }
