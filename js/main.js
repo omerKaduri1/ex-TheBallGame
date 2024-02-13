c51dbad9d3cd98efdbcb6002e98d1c11cb45b3c7
omerKaduri1 <kaduriomer@gmail.com>
2024-02-13 17:14:15 +0200

    Add random color to the ball+improve onBallClick

--- js/main.js
+++ js/main.js
@@ -1,22 +1,18 @@
 'use strict'
 
-var gBallSize= 100
+var gBallSize = 100
 
 function onBallClick() {
     const elBall = document.querySelector('.ball')
+    const randColor = getRandomColor()
     const incrementAmount = getRandomInt(20, 61)
 
     gBallSize += incrementAmount
+    if (gBallSize + incrementAmount > 400) gBallSize = 100
 
-    if (gBallSize + incrementAmount > 400) {
-        elBall.style.width = '100px'
-        elBall.style.height = '100px'
-        elBall.innerText = gBallSize + ''
-        gBallSize = 100
-    }
-
-        elBall.style.width = gBallSize + 'px'
-        elBall.style.height = gBallSize + 'px'
-        elBall.innerText = gBallSize + ''
+    elBall.style.width = gBallSize + 'px'
+    elBall.style.height = gBallSize + 'px'
+    elBall.innerText = gBallSize + ''
+    elBall.style.backgroundColor = randColor
 
 }
